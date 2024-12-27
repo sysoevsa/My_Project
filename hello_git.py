04b9038a26f8b94b80f5e1c29187638160bf5b4f
Sergey Sysoev <us010@mail.ru>
2024-12-27 17:36:22 +1000

    Update testing

--- hello_git.py
+++ hello_git.py
@@ -5,3 +5,6 @@ print('Create new branch testing')
 for num in range(10):
         print(num,end=' ')
 print(f'\nYour current directory: {os.getcwd()}')
+
+s = 'Version Control System'
+print(f'{s} - length is {len(s)} characters')
