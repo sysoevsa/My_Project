e261255b040830d1a5d22ae56df82877561128af
Sergey Sysoev <us010@mail.ru>
2024-12-27 17:20:20 +1000

    Update hello_git py. Add for444

--- hello_git.py
+++ hello_git.py
@@ -1,5 +1,7 @@
+import os
 print('Hello Git')
 print(r"Indexing - i'is important")
 print('Create new branch testing') 
 for num in range(10):
         print(num,end=' ')
+print(f'\nYour current directory: {os.getcwd()}')
