dcfe006a42f7b2b5bf16bb599347271324f11093
Sergey Sysoev <us010@mail.ru>
2024-12-27 17:29:59 +1000

    Update hello_git py. Add for Add While

---
+++ b.py
@@ -0,0 +1,4 @@
+import math
+
+def any_func(a,b):
+        return math.sqrt(a+b)
--- hello_git.py
+++ hello_git.py
@@ -1,2 +1,13 @@
-print('Hello Git')
-print(r"Indexing - i'is important")
+import b
+print('Hello, Git')
+print('Indexing - it is important')
+
+ind = 0
+sum = 0
+
+while ind <= 9:
+        sum += ind
+        print(sum)
+        ind += 1
+
+print(b.any_func(2, sum))
